302ceabd5d2ce117316dbb4393379e6009282259
Angie0488 <43270346+Angie0488@users.noreply.github.com>
2019-09-04 21:38:48 -0400

    Completed Navigation Section

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ const siteContent = {
     "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
+
   "cta": {
     "h1": "DOM Is Awesome",
     "button": "Get Started",
@@ -40,3 +41,12 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// MY CODE HERE
+
+// Navigation Section
+const navigation = document.querySelectorAll('nav a');
+navigation.forEach((link, index) => {
+  link.textContent = siteContent.nav[`nav-item-${index++}`];
+}); 
